905969859df3de1dd951e9960b59e8114ca2e198
Daniel <diversion.con.datos@gmail.com>
2022-05-20 19:10:21 +0200

    [Qtile] Customize mouse actions.

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -343,7 +343,9 @@ mouse = [
          start=lazy.window.get_position()),
     Drag([mod], "Button3", lazy.window.set_size_floating(),
          start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front())
+    Click([mod], "Button1", lazy.window.bring_to_front()),
+    Click([mod], "Button2", lazy.window.kill()),
+    Click([mod, "shift"], "Button3", lazy.window.toggle_floating())
 ]
 
 dgroups_key_binder = None
