b73015cd9c14fca1395f9fd5f94513c33cf658af
Daniel <diversion.con.datos@gmail.com>
2022-05-20 19:32:09 +0200

    [Qtile] Add Picom script.
    This script adds transparency and animations to Qtile.
    
    Requires jonaburg's Picom fork.

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -2,6 +2,9 @@
 
 feh --bg-scale ~/.dotfiles/wallpapers/GralicDogSpace2.jpg
 
+# Compositor.
+picom --experimental-backends --config "${HOME}/.config/qtile/picom.conf" &
+
 # Systray icons.
 nm-applet &
 volumeicon &
