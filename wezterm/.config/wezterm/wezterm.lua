b46c844cfffa9f3efe95b92cd276977ac9033abc
Daniel <diversion.con.datos@gmail.com>
2022-12-25 12:23:44 +0100

    [Wezterm] Change window padding and opacity.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,5 +3,12 @@ local wezterm = require 'wezterm'
 return {
   color_scheme = 'Dracula',
   font = wezterm.font 'CaskaydiaCove NF',
+  window_background_opacity = 0.8,
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
 }
 
