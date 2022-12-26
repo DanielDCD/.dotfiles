5fa98b925417d32a7675e29dc059301b18f3c2dc
Daniel <diversion.con.datos@gmail.com>
2022-12-26 13:20:53 +0100

    [PS] Add some aliases.

--- powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -4,3 +4,14 @@ Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadlineOption -PredictionSource History
 
+# Aliases
+Set-Alias -Name c -Value cls
+
+Set-Alias -Name la -Value Get-ChildItem
+Set-Alias -Name ll -Value Get-ChildItem
+Set-Alias -Name lla -Value Get-ChildItem
+
+Set-Alias -Name vi -Value nvim
+
+Set-Alias -Name cat -Value bat
+
