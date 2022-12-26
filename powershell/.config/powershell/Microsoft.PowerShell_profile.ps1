372039133afed9f8b60f32d0ae9a94aa604bbf9e
Daniel <diversion.con.datos@gmail.com>
2022-12-26 15:21:37 +0100

    [PS] Add zoxide integration.

--- powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -26,6 +26,12 @@ function take {
 
 }
 
+# Zoxide
+Invoke-Expression (& {
+    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+    (zoxide init --hook $hook powershell | Out-String)
+})
+
 # Starship prompt
 Invoke-Expression (&starship init powershell)
 
