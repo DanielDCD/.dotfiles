f59a5c0b2f7e5a4b328facc73f40d0d91fad3f9e
Daniel <diversion.con.datos@gmail.com>
2022-12-26 16:49:53 +0100

    [Wezterm] Fix font name error.
    The previous name was giving some problems to find the font in some
    operating systems.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 return {
   color_scheme = 'Dracula',
-  font = wezterm.font 'CaskaydiaCove NF',
+  font = wezterm.font 'CaskaydiaCove Nerd Font',
   window_background_opacity = 0.8,
   window_padding = {
     left = 0,
