f4dcd07a2044141e2ad821227160d1500ed8c7e2
Daniel <diversion.con.datos@gmail.com>
2022-12-28 21:35:51 +0100

    [Nvim] Setup windows.nvim.
    This plugin lets you maximize and restore the current window or
    automatically expand its width.

---
+++ vim-and-neovim/.config/nvim/lua/plugin/windows.lua
@@ -0,0 +1,11 @@
+vim.o.winwidth = 10
+vim.o.winminwidth = 10
+vim.o.equalalways = false
+
+require('windows').setup()
+
+vim.keymap.set('n', '<C-w>m', '<cmd>WindowsMaximize<CR>')
+vim.keymap.set('n', '<C-w>,', '<cmd>WindowsMaximizeVertically<CR>')
+vim.keymap.set('n', '<C-w>.', '<cmd>WindowsMaximizeHorizontally<CR>')
+vim.keymap.set('n', '<C-w>0', '<cmd>WindowsEqualize<CR>')
+
