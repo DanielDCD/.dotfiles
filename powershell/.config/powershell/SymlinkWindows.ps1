1c1061760ef7b1860535d6100a6d768452ca5e20
Daniel <diversion.con.datos@gmail.com>
2022-12-29 13:31:28 +0100

    [PS] Apply custom Starship configuration.
    Update Windows symlink script to also symlink the Starship prompt
    config file to make it available for PowerShell. It was also necessary
    to fix an error in the config file that made the prompt insert a line
    break between the arrow character and the command.

--- powershell/.config/powershell/SymlinkWindows.ps1
+++ powershell/.config/powershell/SymlinkWindows.ps1
@@ -1,1 +1,4 @@
 New-Item -ItemType HardLink -Path "$HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Target "$PWD\Microsoft.PowerShell_profile.ps1"
+
+New-Item -ItemType HardLink -Path "$HOME\.config\starship.toml" -Target "$HOME\.dotfiles\starship\.config\starship.toml"
+
