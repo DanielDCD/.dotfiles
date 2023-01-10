4c34286448c7c1316724d62e9c40022852f3a388
Daniel <diversion.con.datos@gmail.com>
2023-01-10 11:39:45 +0100

    [Nvim] Configure StyLua with null-ls.
    Configures StyLua source for null-ls and add a command to quickly format
    current buffer.

--- vim-and-neovim/.config/nvim/lua/plugin/null-ls.lua
+++ vim-and-neovim/.config/nvim/lua/plugin/null-ls.lua
@@ -1,5 +1,27 @@
 local null_ls = require("null-ls")
 
+-- Sources
+local code_actions = null_ls.builtins.code_actions
+local diagnostics = null_ls.builtins.diagnostics
+local formatting = null_ls.builtins.formatting
+local hover = null_ls.builtins.hover
+local completion = null_ls.builtins.completion
+
+local sources = {
+
+  -- Formatters
+  formatting.stylua,
+
+}
+
+-- Options
 null_ls.setup({
-    debug = false
+  debug = false,
+  sources = sources,
 })
+
+-- Functions
+vim.api.nvim_create_user_command("FormatBuffer", function()
+  vim.lsp.buf.format()
+end, { nargs = 0 })
+
