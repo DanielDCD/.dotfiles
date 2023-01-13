c2b0ceb59e10415f8312dab2d1e395f328d57b4e
Daniel <diversion.con.datos@gmail.com>
2023-01-13 22:17:29 +0100

    [Nvim] Fix nvim-cmp and Ultisnips issues.
    There was a problem with nvim-cmp that didn't let to jump between
    tabstotps in snippets, even though I added the mappings to do that in
    Ultisnips config file. The solution was to also add that mappings to the
    nvim-cmp config.

--- vim-and-neovim/.config/nvim/lua/plugin/nvim-cmp.lua
+++ vim-and-neovim/.config/nvim/lua/plugin/nvim-cmp.lua
@@ -1,5 +1,6 @@
 local cmp = require 'cmp'
 local lspkind = require 'lspkind'
+local cmp_ultisnips_mappings = require("cmp_nvim_ultisnips.mappings")
 
 cmp.setup({
     snippet = {
@@ -32,7 +33,19 @@ cmp.setup({
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({select = true})
+        ['<CR>'] = cmp.mapping.confirm({select = true}),
+	["<Tab>"] = cmp.mapping(
+          function(fallback)
+            cmp_ultisnips_mappings.expand_or_jump_forwards(fallback)
+          end,
+          {"i", "s", "c"}
+        ),
+        ["<S-Tab>"] = cmp.mapping(
+          function(fallback)
+            cmp_ultisnips_mappings.jump_backwards(fallback)
+          end,
+          {"i", "s", "c"}
+        ),
     }),
     sources = cmp.config.sources({{name = 'nvim_lsp'}, {name = 'ultisnips'}},
                                  {{name = 'buffer'}})
