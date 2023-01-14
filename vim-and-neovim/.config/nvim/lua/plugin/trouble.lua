42bf63058ffeaf43fcdb3e42ee364207c14a2784
Daniel <diversion.con.datos@gmail.com>
2023-01-14 22:57:14 +0100

    [Nvim] Modify trouble.nvim mappings.
    Now the mappings can both open and close the trouble window.

--- vim-and-neovim/.config/nvim/lua/plugin/trouble.lua
+++ vim-and-neovim/.config/nvim/lua/plugin/trouble.lua
@@ -46,21 +46,21 @@ require('trouble').setup {
     use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
 }
 
-vim.api.nvim_set_keymap("n", "<leader>tro", "<cmd>Trouble<cr>",
+vim.api.nvim_set_keymap("n", "<leader>tro", "<cmd>TroubleToggle<cr>",
   {silent = true, noremap = true}
 )
-vim.api.nvim_set_keymap("n", "<leader>trw", "<cmd>Trouble workspace_diagnostics<cr>",
+vim.api.nvim_set_keymap("n", "<leader>trw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
   {silent = true, noremap = true}
 )
-vim.api.nvim_set_keymap("n", "<leader>trd", "<cmd>Trouble document_diagnostics<cr>",
+vim.api.nvim_set_keymap("n", "<leader>trd", "<cmd>TroubleToggle document_diagnostics<cr>",
   {silent = true, noremap = true}
 )
-vim.api.nvim_set_keymap("n", "<leader>trl", "<cmd>Trouble loclist<cr>",
+vim.api.nvim_set_keymap("n", "<leader>trl", "<cmd>TroubleToggle loclist<cr>",
   {silent = true, noremap = true}
 )
-vim.api.nvim_set_keymap("n", "<leader>trq", "<cmd>Trouble quickfix<cr>",
+vim.api.nvim_set_keymap("n", "<leader>trq", "<cmd>TroubleToggle quickfix<cr>",
   {silent = true, noremap = true}
 )
-vim.api.nvim_set_keymap("n", "gR", "<cmd>Trouble lsp_references<cr>",
+vim.api.nvim_set_keymap("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
   {silent = true, noremap = true}
 )
