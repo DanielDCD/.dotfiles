be5317f2ca9dc41c53e17961b1757a6c9473d3d1
Daniel <diversion.con.datos@gmail.com>
2023-01-14 20:03:14 +0100

    Revert "[Nvim] Show line diagnostics in hover window."
    This reverts commit 9cf70f2224e3b611ba515f2c83fa4031383a4a50.
    
    I need virtual text back instead of the hover window for LSP diagnostics
    because it causes some conflicts with plugins that also use a hover
    window.

--- vim-and-neovim/.config/nvim/lua/plugin/nvim-lspconfig.lua
+++ vim-and-neovim/.config/nvim/lua/plugin/nvim-lspconfig.lua
@@ -1,30 +1,5 @@
 local lspconfig = require("lspconfig")
 
--- Options
-vim.o.updatetime = 250
-vim.api.nvim_create_autocmd("CursorHold", {
-    buffer = bufnr,
-    callback = function()
-	local opts = {
-	    focusable = false,
-	    close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-	    border = 'rounded',
-	    source = 'always',
-	    prefix = ' ',
-	    scope = 'cursor',
-	}
-	vim.diagnostic.open_float(nil, opts)
-    end
-})
-
-vim.diagnostic.config({
-    virtual_text = false,
-    signs = true,
-    underline = true,
-    update_in_insert = true,
-    severity_sort = false,
-})
-
 -- Setup language servers
 local mason_lspconfig = require("mason-lspconfig")
 mason_lspconfig.setup()
@@ -65,4 +40,3 @@ map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
 vim.cmd('autocmd BufWritePre *.js lua vim.lsp.buf.formatting_sync(nil, 100)')
 vim.cmd('autocmd BufWritePre *.jsx lua vim.lsp.buf.formatting_sync(nil, 100)')
 vim.cmd('autocmd BufWritePre *.py lua vim.lsp.buf.formatting_sync(nil, 100)')
-
