3d693d3cf1733df8c5ec3c57628eabca42e2ca63
Daniel <diversion.con.datos@gmail.com>
2024-07-09 17:16:02 +0200

    [Nvim] Remove colors of indent-blankline.

--- vim-and-neovim/.config/nvim/lua/plugin/indent-blankline.lua
+++ vim-and-neovim/.config/nvim/lua/plugin/indent-blankline.lua
@@ -13,24 +13,6 @@ local highlight = {
   'RainbowCyan',
 }
 
-local colors = require('dracula').colors()
-
-local hooks = require('ibl.hooks')
-
-hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-  vim.api.nvim_set_hl(0, 'RainbowRed', { fg = colors.red })
-  vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = colors.yellow })
-  vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = colors.bright_blue })
-  vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = colors.orange })
-  vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = colors.green })
-  vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = colors.purple })
-  vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = colors.cyan })
-end)
-
-require('ibl').setup({ indent = { highlight = highlight } })
-
-local options = {
-  indent = { highlight = highlight },
-}
+options = {}
 
 ibl.setup(options)
